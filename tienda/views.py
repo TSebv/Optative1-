c2e33d45869e08b6961ee9142d82bf75f000b815
TSebv <113436744+TSebv@users.noreply.github.com>
2026-02-27 00:32:47 +0000

    Mis cambios

--- tienda/views.py
+++ tienda/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Producto,Pedido,cliente
 
 def home(request):
     return render(request, "tienda/home.html", {})
+
+"""
+Vista para listar productos
+"""
+def lista_productos(request):
+    productos = Producto.objects.all().order_by("nombre")
+    return render(request, "tienda/lista_productos.html", {"productos":productos})
